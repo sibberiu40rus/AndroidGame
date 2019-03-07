66f160d9224a3ae236341ec627bb449198b3f328
siberiu40rus <siberiu40rus@gmail.com>
2019-03-07 12:53:42 +0300

    add_background

--- AndroidApp/core/src/ru/geekbrains/stargame/StarGame.java
+++ AndroidApp/core/src/ru/geekbrains/stargame/StarGame.java
@@ -13,12 +13,12 @@ public class StarGame extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("background.jpg");
+		img = new Texture("backggroun_space.jpg");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 1, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
