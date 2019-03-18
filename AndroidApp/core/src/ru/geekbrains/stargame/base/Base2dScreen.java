91033700a35b42674dac749a2ee7a511ca719624
martin <siberiu40rus@gmail.com>
2019-03-18 09:18:22 +0300

    done homework

--- AndroidApp/core/src/ru/geekbrains/stargame/base/Base2dScreen.java
+++ AndroidApp/core/src/ru/geekbrains/stargame/base/Base2dScreen.java
@@ -3,13 +3,38 @@ package ru.geekbrains.stargame.base;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Matrix3;
+import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Vector2;
+
+import ru.geekbrains.stargame.math.MatrixUtils;
+import ru.geekbrains.stargame.math.Rect;
 
 public abstract class Base2dScreen implements Screen, InputProcessor {
+
+    protected SpriteBatch batch;
+
+    private Matrix4 worldToGl;
+    private Matrix3 screenToWorld;
+
+    private Rect screenBounds;  // границы области рисования в пикселях
+    private Rect worldBounds;   // границы проекции мировых координат
+    private Rect glBounds;      // квадрат OpenGL
+
+    private Vector2 touch;
 
     @Override
     public void show() {
         System.out.println("show");
+        batch = new SpriteBatch();
         Gdx.input.setInputProcessor(this);
+        this.worldToGl = new Matrix4();
+        this.screenToWorld = new Matrix3();
+        this.screenBounds = new Rect();
+        this.worldBounds = new Rect();
+        this.glBounds = new Rect(0, 0, 1f, 1f);
+        this.touch = new Vector2();
     }
 
     @Override
@@ -19,7 +44,22 @@ public abstract class Base2dScreen implements Screen, InputProcessor {
 
     @Override
     public void resize(int width, int height) {
-       // System.out.println("resize width = " + width + " height = " + height);
+        System.out.println("resize width = " + width + " height = " + height);
+        screenBounds.setSize(width, height);
+        screenBounds.setLeft(0);
+        screenBounds.setBottom(0);
+
+        float aspect = width / (float) height;
+        worldBounds.setHeight(1f);
+        worldBounds.setWidth(1f * aspect);
+        MatrixUtils.calcTransitionMatrix(worldToGl, worldBounds, glBounds);
+        batch.setProjectionMatrix(worldToGl);
+        MatrixUtils.calcTransitionMatrix(screenToWorld, screenBounds, worldBounds);
+        resize(worldBounds);
+    }
+
+    public void resize(Rect worldBounds) {
+        System.out.println("resize worldBounds.width = " + worldBounds.getWidth() + " worldBounds.height = " + worldBounds.getHeight());
     }
 
     @Override
@@ -40,12 +80,13 @@ public abstract class Base2dScreen implements Screen, InputProcessor {
 
     @Override
     public void dispose() {
-       // System.out.println("dispose");
+        batch.dispose();
+        System.out.println("dispose");
     }
 
     @Override
     public boolean keyDown(int keycode) {
-       System.out.println("keyDown keycode = " + keycode);
+        System.out.println("keyDown keycode = " + keycode);
         return false;
     }
 
@@ -57,25 +98,46 @@ public abstract class Base2dScreen implements Screen, InputProcessor {
 
     @Override
     public boolean keyTyped(char character) {
-       // System.out.println("keyTyped character = " + character);
+        // System.out.println("keyTyped character = " + character);
         return false;
     }
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        //System.out.println("touchDown screenX = " + screenX + " screenY = " + screenY);
+        touch.set(screenX, Gdx.graphics.getHeight() - screenY).mul(screenToWorld);
+        System.out.println("touchDown screenX = " + screenX + " screenY = " + screenY);
+        touchDown(touch, pointer);
+        return false;
+    }
+
+    public boolean touchDown(Vector2 touch, int pointer) {
+        System.out.println("touchDown touch.x = " + touch.x + " touch.y = " + touch.y);
         return false;
     }
 
     @Override
     public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-      //  System.out.println("touchUp screenX = " + screenX + " screenY = " + screenY);
+        touch.set(screenX, Gdx.graphics.getHeight() - screenY).mul(screenToWorld);
+        System.out.println("touchUp screenX = " + screenX + " screenY = " + screenY);
+        touchUp(touch, pointer);
+        return false;
+    }
+
+    public boolean touchUp(Vector2 touch, int pointer) {
+        System.out.println("touchUp touch.x = " + touch.x + " touch.y = " + touch.y);
         return false;
     }
 
     @Override
     public boolean touchDragged(int screenX, int screenY, int pointer) {
-      //  System.out.println("touchDragged screenX = " + screenX + " screenY = " + screenY);
+        touch.set(screenX, Gdx.graphics.getHeight() - screenY).mul(screenToWorld);
+        System.out.println("touchDragged screenX = " + screenX + " screenY = " + screenY);
+        touchDragged(touch, pointer);
+        return false;
+    }
+
+    public boolean touchDragged(Vector2 touch, int pointer) {
+        System.out.println("touchDragged touch.x = " + touch.x + " touch.y = " + touch.y);
         return false;
     }
 
--- AndroidApp/core/src/ru/geekbrains/stargame/screen/MenuScreen.java
+++ AndroidApp/core/src/ru/geekbrains/stargame/screen/MenuScreen.java
@@ -3,131 +3,57 @@ package ru.geekbrains.stargame.screen;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
 import ru.geekbrains.stargame.base.Base2dScreen;
+import ru.geekbrains.stargame.math.Rect;
+import ru.geekbrains.stargame.sprites.Background;
 
 
 public class MenuScreen extends Base2dScreen {
-    private SpriteBatch batch;
-    private Texture img;
-    private Texture spaceShip;
-    private Vector2 touch;
+
     private Vector2 pos;
-    private Vector2 v;
-    private Vector2 v2;
-    private Vector2 route;
-    private float speed = 4.0f;
-    private int key = 0;
+
+    private Background background;
+    private Texture backgroundTexture;
 
 
 
     @Override
     public void show() {
         super.show();
-        batch = new SpriteBatch();
-        img = new Texture("background.jpg");
-        spaceShip = new Texture("spaceShip.png");
-        touch = new Vector2();
-        route = new Vector2();
+
+        backgroundTexture = new Texture("background.jpg");
+        background = new Background(new TextureRegion(backgroundTexture));
         pos = new Vector2(0, 0);
-        v = new Vector2(0f, 0f);
-        v2 = new Vector2(0f, 0f);
+
     }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        super.resize(worldBounds);
+        background.resize(worldBounds);
+    }
+
 
 
     @Override
     public void render(float delta) {
-
         super.render(delta);
-        Gdx.gl.glClearColor(1, 1, 0, 1);
+
+        Gdx.gl.glClearColor(0, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        batch.draw(img, 0, 0);
-        batch.draw(spaceShip, pos.x, pos.y);
+        background.draw(batch);
         batch.end();
-        route.set(touch);
-
-        if (v2.x == 0 && v2.y == 0) {
-            if (route.sub(pos).len() <= speed) {
-                pos.set(touch);
-            } else {
-                pos.add(v);
-            }
-        }pos.add(v2);
 
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
-        img.dispose();
-        spaceShip.dispose();
         super.dispose();
+        backgroundTexture.dispose();
     }
 
-    public void moveWithKeyDown () {
-
-    }
-
-    @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        touch.set(screenX, Gdx.graphics.getHeight() - screenY);
-        v.set(touch.cpy().sub(pos)).setLength(speed);
-        System.out.println("touch x = " + touch.x + " touch.y = " + touch.y);
-        return super.touchDown(screenX, screenY, pointer, button);
-    }
-
-    /**
-     * немного криво, дальше обязательно додумаю по лучше((
-     * @param keycode
-     * @return
-     */
-
-    @Override
-    public boolean keyDown(int keycode) {
-        key = keycode;
-        switch (key) {
-            case (22):
-                v2.set(1,0).setLength(speed);
-                break;
-            case (21):
-                v2.set(-1,0).setLength(speed);
-                break;
-            case (19):
-                v2.set(0,1).setLength(speed);
-                break;
-            case (20):
-                v2.set(0,-1).setLength(speed);
-                break;
-        }
-        System.out.println("keyDown keycode = " + keycode);
-        return false;
-    }
-
-    @Override
-    public boolean keyUp(int keycode) {
-        key = keycode;
-        switch (key) {
-            case (22):
-                v2.set(0,0);
-               // key = 0;
-                break;
-            case (21):
-                v2.set(0,0);
-                //key = 0;
-                break;
-            case (19):
-                v2.set(0,0);
-               // key = 0;
-                break;
-            case (20):
-                v2.set(0,0);
-               // key = 0;
-                break;
-        }
-        System.out.println("keyDown keycode = " + keycode);
-        return false;
-    }
 }
