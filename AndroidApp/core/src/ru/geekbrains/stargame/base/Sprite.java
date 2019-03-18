6429a0aa2c9c5f4ae9ce755aae7f3694d3ea6f3a
martin <siberiu40rus@gmail.com>
2019-03-18 09:10:33 +0300

    done homework

--- AndroidApp/core/src/ru/geekbrains/stargame/base/Sprite.java
+++ AndroidApp/core/src/ru/geekbrains/stargame/base/Sprite.java
@@ -2,6 +2,7 @@ package ru.geekbrains.stargame.base;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 
 import ru.geekbrains.stargame.math.Rect;
 
@@ -26,5 +27,39 @@ public class Sprite extends Rect {
                 scale, scale,
                 angle
         );
+    }
+
+    public void setProportion(float height) {
+        setHeight(height);
+        float aspect = regions[frame].getRegionWidth() / (float) regions[frame].getRegionHeight();
+        setWidth(height * aspect);
+    }
+
+    public void resize(Rect worldBounds) {
+
+    }
+
+    public boolean touchDown(Vector2 touch, int pointer) {
+        return false;
+    }
+
+    public boolean touchUp(Vector2 touch, int pointer) {
+        return false;
+    }
+
+    public float getAngle() {
+        return angle;
+    }
+
+    public void setAngle(float angle) {
+        this.angle = angle;
+    }
+
+    public float getScale() {
+        return scale;
+    }
+
+    public void setScale(float scale) {
+        this.scale = scale;
     }
 }
--- AndroidApp/core/src/ru/geekbrains/stargame/sprites/Background.java
+++ AndroidApp/core/src/ru/geekbrains/stargame/sprites/Background.java
@@ -3,9 +3,16 @@ package ru.geekbrains.stargame.sprites;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 import ru.geekbrains.stargame.base.Sprite;
+import ru.geekbrains.stargame.math.Rect;
 
 public class Background extends Sprite {
     public Background(TextureRegion region) {
         super(region);
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        setProportion(worldBounds.getHeight());
+        pos.set(worldBounds.pos);
     }
 }
